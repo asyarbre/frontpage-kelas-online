695a0f8ad50f80b157904b16c0eb2f3b9ebd003a
Asya'ri <asyari818@gmail.com>
2023-02-24 20:44:00 +0700

    Set Up Axios

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import axios from "../configs/axios";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+function Home(props) {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-400">
-      Hello world!
-    </h1>
-  )
+    <h1 className="text-3xl font-bold underline text-blue-400">Hello world!</h1>
+  );
 }
+
+// getinitialprops to get data from server url is /courses
+Home.getInitialProps = async () => {
+  try {
+    const data = await axios.get(`/courses`);
+    return {data: data.data.data};
+  }
+  catch (error) {
+    return error;
+  }
+};
+
+
+export default Home;
