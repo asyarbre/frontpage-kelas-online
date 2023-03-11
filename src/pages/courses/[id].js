e2f063f170ad063c080cacd121e21ede9e8ef5e8
Asya'ri <asyari818@gmail.com>
2023-03-11 17:31:42 +0700

    [Course Detail] Add Course title

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Youtube from "react-youtube";
 
 import courses from "src/constants/api/courses";
+import Header from "../../parts/Header";
 
 function DetailsCourse({ data }) {
   console.log(data);
@@ -11,7 +12,10 @@ function DetailsCourse({ data }) {
       <Head>
         <title>Details Course</title>
       </Head>
-      <section className="pt-10 relative overflow-hidden" style={{ height: 660 }}>
+      <section
+        className="pt-10 relative overflow-hidden"
+        style={{ height: 660 }}
+      >
         {data?.chapters?.[0]?.lessons?.[0]?.video && (
           <div className="video-wrapper min-h-screen md:min-h-full">
             <Youtube
@@ -32,6 +36,18 @@ function DetailsCourse({ data }) {
             ></Youtube>
           </div>
         )}
+        <div className="absolute inset-0 z-0 w-full h-full bg-black opacity-75"></div>
+        <div className="meta-title absolute inset-0 object-fill z-0 w-full flex justify-center items-center">
+          <div className="text-center">
+            <h3 className="text-lg text-white">Kelas Online: </h3>
+            <h4 className="text-3xl md:text-6xl text-teal-500 font-semibold">
+              {data?.name ?? "Nama Kelas"}
+            </h4>
+          </div>
+        </div>
+        <div className="container mx-auto z-10 relative">
+          <Header />
+        </div>
       </section>
     </>
   );
